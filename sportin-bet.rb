d5f080226409e818cf2fec72281bca227035dafa
Matheus Silva <msilva.jus@gmail.com>
2019-06-22 20:45:19 -0300

    msilvadev
    Removido linhas desnecessárias
    Adicionado comentario

--- sportin-bet.rb
+++ sportin-bet.rb
@@ -12,6 +12,8 @@ def bet()
     request["Origin"] = "https://d1m9u7748ksoao.cloudfront.net" 
     request["Connection"] = "keep-alive" 
     request["Referer"] = "https://d1m9u7748ksoao.cloudfront.net/casino/playtechpop/tigersglory/index.html?partnerId=402&market=gb&remoteusername=&gameid=tigersglory&kinid=sportingbet.com&ticket=&moneymode=fun&lang=pt&currency=USD&imsgame=pop_tigersglory_qsp&lobbyPage=https%253A%252F%252Fcasinogames.sportingbet.com%252Ffcasino%252Fen_US%252Ffcgames%252Fvendorgames%252Fhome.html&relaunchurl=https://gamelauncher-uu-pop-com.vegasgames.sportingbet.com/launcher?real%3D0%26casino%3Dsportingbet.com%26clienttype%3Dcasino%26lobbyPage%3Dhttps://casinogames.sportingbet.com/fcasino/en_US/fcgames/vendorgames/home.html%26brandName%3Dsportingbet.com%26clientplatform%3Dweb%26language%3DPT%26depositPage%3Dhttps://casinogames.sportingbet.com/fcasino/en_US/fcgames/vendorgames/cashier.html%26imsgame%3Dpop_tigersglory_qsp&brandName=sportingbet.com&depositPage=https%253A%252F%252Fcasinogames.sportingbet.com%252Ffcasino%252Fen_US%252Ffcgames%252Fvendorgames%252Fcashier.html&crosslaunchurl=https://gamelauncher-uu-pop-com.vegasgames.sportingbet.com/launcher?real%3D0%26casino%3Dsportingbet.com%26clienttype%3Dcasino%26lobbyPage%3Dhttps://casinogames.sportingbet.com/fcasino/en_US/fcgames/vendorgames/home.html%26brandName%3Dsportingbet.com%26clientplatform%3Dweb%26language%3DPT%26depositPage%3Dhttps://casinogames.sportingbet.com/fcasino/en_US/fcgames/vendorgames/cashier.html%26imsgame%3Dpop_tigersglory_qsp" 
+   
+    #Dados necessários para realizar a aposta
     request.body = JSON.dump({ 
         "sid" => "ea6a23523e2ff85304a52c9f440dc677e7221468", 
         "g" => "tigersglory", 
